48be87a793cf09ee9fedd4302daf91f1b3682c0e
Andrew Pelykh <andrew.pelykh@gmail.com>
2017-05-23 10:08:14 +0300

    Create games

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,26 @@
+class GamesController < ApplicationController
+
+  def index
+    @game = Game.new
+    @games = Game.all
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to @game, notice: 'Game was successfully created.'
+    else
+      redirect_to games_path
+    end
+  end
+
+  private
+
+    def game_params
+      params.require(:game).permit(:title)
+    end
+end
