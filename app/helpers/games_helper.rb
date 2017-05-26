99070a1caf254ce80fa25841ccbf00980cb05ae1
Andrew Pelykh <andrew.pelykh@gmail.com>
2017-05-26 14:15:21 +0300

    Change textures, add lava audio

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,15 +1,15 @@
 module GamesHelper
   def point(path, point)
     if point.start
-      %{<div class="point start p_#{point.x}_#{point.y}"></div>}.html_safe
+      image_tag("start.png", height: '32', width: '32', class: "p_#{point.x}_#{point.y}" ).html_safe
     elsif point.goal
-      %{<div class="point goal p_#{point.x}_#{point.y}"></div>}.html_safe
+      image_tag("goal.png", height: '32', width: '32', class: "p_#{point.x}_#{point.y}" ).html_safe
     elsif point.busy
       image_tag("busy.png", height: '32', width: '32', class: "p_#{point.x}_#{point.y}" ).html_safe
     elsif is_path? path, point
       image_tag("path.png", height: '32', width: '32', class: "p_#{point.x}_#{point.y}").html_safe
     else
-      image_tag("point.png", height: '32', width: '32', class: "p_#{point.x}_#{point.y}").html_safe
+      image_tag("point.gif", height: '32', width: '32', class: "p_#{point.x}_#{point.y}").html_safe
     end
   end
 
